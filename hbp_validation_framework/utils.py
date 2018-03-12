8f36c23d9bdce34147a30ff2dcc941180842a7f7
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2018-03-12 15:19:54 +0100

    utils.run_test(): Update model instance finding

--- hbp_validation_framework/utils.py
+++ hbp_validation_framework/utils.py
@@ -182,11 +182,18 @@ def run_test(hbp_username="", environment="production", model="", test_instance_
             model_instance_uuid = None
             if getattr(score.model, 'model_uuid', False):
                 # no `score.model.model_instance_uuid` but `score.model.model_uuid` present
-                if getattr(score.model, 'model_hash', False):
-                    # check to find if a model instance with same hash value exists
-                    # if yes then use its model_instance_uuid
-                    model_instances = model_catalog.list_model_instances(model_id=score.model.model_uuid)
-                    if len(model_instances) > 0:
+                model_instances = model_catalog.list_model_instances(model_id=score.model.model_uuid)
+                if len(model_instances) > 0:
+                    if getattr(score.model, 'version', False):
+                        # check to find if a model instance with same version exists
+                        # if yes then use its model_instance_uuid
+                        for m_inst in model_instances:
+                            if m_inst["version"] == score.model.version:
+                                model_instance_uuid = m_inst["id"]
+                    if not model_instance_uuid and getattr(score.model, 'model_hash', False):
+                        # if no matching model instance was found using version, then:
+                        # check to find if a model instance with same hash value exists
+                        # if yes then use its model_instance_uuid
                         for m_inst in model_instances:
                             if m_inst["description"] == score.model.model_hash:
                                 model_instance_uuid = m_inst["id"]
