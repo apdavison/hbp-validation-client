0e7751a63d8e814d1bd272293147dfb7cdb436da
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2019-02-08 11:44:19 +0100

    Update register result

--- hbp_validation_framework/__init__.py
+++ hbp_validation_framework/__init__.py
@@ -1238,10 +1238,10 @@ class TestLibrary(BaseClient):
                         "test_code_id": test_result.test.uuid,
                         "results_storage": results_storage,
                         "score": test_result.score,
-                        "runtime": test_result.runtime,
+                        "runtime": getattr(test_result, 'runtime', None),
                         "passed": None if "passed" not in test_result.related_data else test_result.related_data["passed"],
                         "platform": str(self._get_platform()), # database accepts a string
-                        "hash": test_result.score_hash,
+                        "hash": getattr(test_result, 'score_hash', None),
                         "project": project,
                         "normalized_score": test_result.score
                       }
