ffab6a17bfb1f422f441f3b523c3ef1a750de721
Shailesh Appukuttan <appukuttan.shailesh@gmail.com>
2020-07-15 18:53:58 +0200

    start implementation for collab v2 datastore

--- hbp_validation_framework/datastores.py
+++ hbp_validation_framework/datastores.py
@@ -3,6 +3,7 @@ Defines classes for interacting with remote data stores.
 
 Currently supported:
     - HBP Collaboratory storage
+    - HBP Collaboratory v2 seafile storage
     - Swift CSCS storage
     - ModelDB
 
@@ -53,6 +54,175 @@ class FileSystemDataStore(object):
     def load_data(self, local_path):
         with open(local_path) as fp:
             observation_data = json.load(fp)
+
+
+class SeaFileDataStore(object):
+    """
+    A class for uploading and downloading data from HBP Collaboratory v2 seafile storage.
+    """
+
+    def __init__(self, project_id=None, base_folder=None, auth=None, **kwargs):
+        self.project_id = project_id
+        self.base_folder = base_folder
+        self._auth = auth  # we defer authorization until needed
+        self._authorized = False
+
+    @property
+    def authorized(self):
+        return self._authorized
+
+    def authorize(self, auth=None):
+        if auth is None:
+            auth = self._auth
+        self.doc_client = ApiClient.new(auth.token)
+        self._authorized = True
+
+    def _translate_URL_to_UUID(self, path):
+        """
+        Can take a path such as `collab://5165/hippoCircuit_20171027-142713`
+        with 5165 being the Collab ID and the latter part being the collab path,
+        and translate this to the UUID on the HBP Collaboratory storage.
+        """
+        if not self.authorized:
+            self.authorize(self._auth)
+        entity = self.doc_client.get_entity_by_query(path=path)
+        return entity["uuid"]
+
+    def _translate_UUID_to_URL(self, uuid):
+        """
+        Can take a UUID on the HBP Collaboratory storage path and translate this
+        to a path such as `collab://5165/hippoCircuit_20171027-142713` with 5165
+        being the Collab ID and the latter part being the collab storage path.
+        """
+        if not self.authorized:
+            self.authorize(self._auth)
+        path = self.doc_client.get_entity_path(uuid)
+        return "collab:/{}".format(path)
+
+    def upload_data(self, file_paths):
+        if not self.authorized:
+            self.authorize(self._auth)
+        projects_in_collab = self.doc_client.list_projects(collab_id=self.project_id,
+                                                           access='write')["results"]
+        assert len(projects_in_collab) == 1
+        project_id = projects_in_collab[0]["uuid"]
+        base_folder_id = self._make_folders(self.base_folder, parent=project_id)
+
+        if len(file_paths) > 1:
+            common_base_dir = os.path.dirname(os.path.commonprefix(file_paths))
+        else:
+            common_base_dir = os.path.dirname(file_paths[0])
+        relative_paths = [os.path.relpath(p, common_base_dir) for p in file_paths]
+
+        cached_folders = {}  # avoid unnecessary network calls
+        for local_path, relative_path in zip(file_paths, relative_paths):
+
+            parent = base_folder_id
+            folder_path = os.path.dirname(relative_path)
+
+            # temporary fix: all files in a single directory in Collab storage
+            # if subdirs, then saved filename = `subdir1_subdir2_....subdirN_filename`
+            name = ""
+            if folder_path:  # if there are subdirectories...
+                for subdir in folder_path.split("/"):
+                    name  = name + subdir + "_"
+                """
+                if folder_path in cached_folders:
+                    parent = cached_folders[folder_path]
+                else:
+                    parent = self._make_folders(folder_path, parent=parent)
+                    cached_folders[folder_path] = parent
+                """
+
+            name = name + os.path.basename(relative_path)
+            content_type = mimetypes.guess_type(local_path)[0]
+            file_entity = self.doc_client.create_file(name, content_type, parent)
+            etag = self.doc_client.upload_file_content(file_entity['uuid'],
+                                                       source=local_path)
+
+        return "collab:/{}".format(self.doc_client.get_entity_path(base_folder_id))
+
+    def _make_folders(self, folder_path, parent):
+        for i, folder_name in enumerate(folder_path.split(os.path.sep)):
+            folders = self.doc_client.list_folder_content(parent, entity_type="folder")["results"]
+            folder_exists = False
+            for f in folders:
+                if folder_name in f["name"]:
+                    child = f['uuid']
+                    folder_exists = True
+                    break
+            if not folder_exists:
+                child = self.doc_client.create_folder(folder_name, parent=parent)['uuid']
+            parent = child
+        return child
+
+    def _download_data_content(self, remote_path):
+        if not self.authorized:
+            self.authorize(self._auth)
+        # need to support other formats besides JSON
+        if remote_path.startswith("collab:/"):
+            remote_path = remote_path[len("collab:/"):]
+
+        entity = self.doc_client.get_entity_by_query(path=remote_path)
+        if entity["entity_type"] == 'file':
+            etag, content = self.doc_client.download_file_content(entity["uuid"])
+        else:
+            raise IOError("Can only load data from individual files, not from {}".format(entity["entity_type"]))
+        return content
+
+    def download_data(self, remote_paths, local_directory="."):
+        """
+        Note: This can only download files (not directories)
+        """
+        if isinstance(remote_paths, str):
+            remote_paths = [remote_paths]
+        local_paths = []
+        for remote_path in remote_paths:
+            local_path = os.path.join(local_directory, os.path.basename(remote_path))
+            Path(os.path.dirname(local_path)).mkdir(parents=True, exist_ok=True)
+            with open(local_path, "wb") as fp:
+                fp.write(self._download_data_content(remote_path))
+            local_paths.append(local_path)
+        return local_paths
+
+    def download_data_using_uuid(self, uuid, local_directory="."):
+        """
+        Downloads the resource specified by the UUID on the HBP Collaboratory.
+        Target can be a file or a folder. Returns a list containing absolute
+        filepaths of all downloaded files.
+        Note: This can recursively download files and sub-directories
+              within a specified directory
+        """
+        file_uuids = []
+
+        if not self.authorized:
+            self.authorize(self._auth)
+        entity = self.doc_client.get_entity_details(uuid)
+        if entity["entity_type"] == 'file':
+            file_uuids.append(uuid)
+        elif entity["entity_type"] == 'folder':
+            items = self.doc_client.list_folder_content(uuid)["results"]
+            for item in items:
+                file_uuids.extend(self.download_data_using_uuid(item["uuid"], local_directory=os.path.join(local_directory, entity["name"])))
+            return file_uuids
+        else:
+            raise Exception("Downloading of resources currently supported only for files and folders!")
+
+        remote_paths = []
+        local_paths = []
+        for uuid in file_uuids:
+            remote_paths.append(self._translate_UUID_to_URL(uuid))
+        local_paths.extend(self.download_data(remote_paths=remote_paths, local_directory=local_directory))
+        return local_paths
+
+    def load_data(self, remote_path):
+        content = self._download_data_content(remote_path)
+        content_type = mimetypes.guess_type(remote_path)[0]
+        if content_type == "application/json":
+            return json.loads(content)
+        else:
+            return content
+
 
 
 class CollabDataStore(object):
@@ -350,6 +520,7 @@ class SwiftDataStore(object):
 
 URI_SCHEME_MAP = {
     "collab": CollabDataStore,
+    "collab_v2": SeaFileDataStore,
     "http": HTTPDataStore,
     "https": HTTPDataStore,
     "swift": SwiftDataStore
