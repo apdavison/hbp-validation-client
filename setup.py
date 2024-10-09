b2de71ccac83cf82b634b48581a121e125c26aae
Andrew Davison <andrew.davison@cnrs.fr>
2024-10-09 13:53:10 +0200

    Move to using pyproject.toml

--- setup.py
+++ setup.py
@@ -1,34 +1,38 @@
-import os
 from setuptools import setup
 
+setup()
 
-def package_files(base_dir, directory):
-    pwd = os.getcwd()
-    os.chdir(base_dir)
-    paths = []
-    for (path, directories, filenames) in os.walk(directory):
-        for filename in filenames:
-            paths.append(os.path.join(path, filename))
-    os.chdir(pwd)
-    return paths
+# import os
+# from setuptools import setup
 
 
-json_files = package_files("ebrains_validation_framework", "jsonTreeViewer")
-template_files = package_files("ebrains_validation_framework", "templates")
+# def package_files(base_dir, directory):
+#     pwd = os.getcwd()
+#     os.chdir(base_dir)
+#     paths = []
+#     for (path, directories, filenames) in os.walk(directory):
+#         for filename in filenames:
+#             paths.append(os.path.join(path, filename))
+#     os.chdir(pwd)
+#     return paths
 
-setup(
-    name="ebrains_validation_framework",
-    version="0.8.3",
-    packages=["ebrains_validation_framework"],
-    package_data={"": json_files + template_files},
-    url="https://github.com/HumanBrainProject/hbp-validation-client",
-    license="BSD",
-    author="Andrew Davison and Shailesh Appukuttan",
-    author_email="andrew.davison@cnrs.fr, appukuttan.shailesh@gmail.com",
-    description="Python client for the EBRAINS Validation Framework web services",
-    install_requires=["requests", "nameparser", "ebrains_drive"],
-    extras_require={
-        "reports": ["Jinja2", "pyppdf", "beautifulsoup4", "hbp_archive"],
-        "utils": ["sciunit"],
-    },
-)
+
+# json_files = package_files("ebrains_validation_framework", "jsonTreeViewer")
+# template_files = package_files("ebrains_validation_framework", "templates")
+
+# setup(
+#     name="ebrains_validation_framework",
+#     version="0.9.0.dev",
+#     packages=["ebrains_validation_framework"],
+#     package_data={"": json_files + template_files},
+#     url="https://github.com/HumanBrainProject/ebrains-validation-client",
+#     license="BSD",
+#     author="Andrew Davison and Shailesh Appukuttan",
+#     author_email="andrew.davison@cnrs.fr, appukuttan.shailesh@gmail.com",
+#     description="Python client for the EBRAINS Validation Framework web services",
+#     install_requires=["requests", "nameparser", "ebrains_drive"],
+#     extras_require={
+#         "reports": ["Jinja2", "pyppdf", "beautifulsoup4", "hbp_archive"],
+#         "utils": ["sciunit"],
+#     },
+# )
